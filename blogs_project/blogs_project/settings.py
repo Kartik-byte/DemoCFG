96f7b273c98131165a7558f5a0b4be38c734e1d2
Nidhi Thakkar <78204467+nidhi-o20@users.noreply.github.com>
2022-06-10 10:37:28 +0530

    Update settings.py

--- blogs_project/blogs_project/settings.py
+++ blogs_project/blogs_project/settings.py
@@ -138,5 +138,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = '2019nidhi.thakkar@ves.ac.in'
-EMAIL_HOST_PASSWORD = 'hkgnsvwmebukmofy'
+EMAIL_HOST_USER = '<EMAIL>'
+EMAIL_HOST_PASSWORD = '<PASSWORD>'
